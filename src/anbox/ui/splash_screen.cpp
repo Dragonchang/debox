ea229cb29a82952495d025d7c75503bb3e5d2e04
zhangfl <zhangfl@deepblueai.com>
2021-11-17 18:19:36 +0800

    fix issue

--- src/anbox/ui/splash_screen.cpp
+++ src/anbox/ui/splash_screen.cpp
@@ -15,15 +15,6 @@
  *
  */
 
-#include<sys/types.h>
-#include<sys/stat.h>
-#include<sys/mman.h>
-#include<sys/ioctl.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<linux/fb.h>
-
-
 #include "anbox/ui/splash_screen.h"
 #include "anbox/system_configuration.h"
 #include "anbox/utils.h"
@@ -44,20 +35,8 @@ SplashScreen::SplashScreen() {
     BOOST_THROW_EXCEPTION(std::runtime_error(message));
   }
 
-  int fd;
-  struct fb_var_screeninfo screen_info;
-  fd = open("/dev/fb0", O_RDWR);
-  ioctl(fd, FBIOGET_VSCREENINFO, &screen_info);
-  printf("%d*%d\n", screen_info.xres, screen_info.yres);
-  close(fd);
-  int width = screen_info.xres;
-  int height = screen_info.yres;
-  WARNING("window size '%d','%d'", width, height);
-  if (width <= 0 || height <=0) {
-	  width = 1024;
-	  height = 768;
-  }
-  window_ = SDL_CreateWindow("", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+  const auto width = 1024, height = 768;
+  window_ = SDL_CreateWindow("", 0, 0,
                              width, height, SDL_WINDOW_OPENGL | SDL_WINDOW_BORDERLESS | SDL_WINDOW_FULLSCREEN_DESKTOP);
   if (!window_) {
     const auto message = utils::string_format("Failed to create window: %s", SDL_GetError());
